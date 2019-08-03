3cdb9184e22997d96c690a4d9d140d6ba5a253e9
Jim Saari <jim.saari@livion.fi>
2019-08-03 13:42:07 +0300

    Add musalink

---
+++ musalink/precache-manifest.e5f92dbf86a195217b782473e741a368.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0306ebd7d551d9aa36c16512221c611d",
+    "url": "/musalink/index.html"
+  },
+  {
+    "revision": "8dacdcf4898532097a52",
+    "url": "/musalink/static/js/2.6ef34078.chunk.js"
+  },
+  {
+    "revision": "66645f32501ec6e0d98e",
+    "url": "/musalink/static/js/main.446303c5.chunk.js"
+  },
+  {
+    "revision": "ed3bef8da9c1c387e6c8",
+    "url": "/musalink/static/js/runtime~main.e1153d6a.js"
+  },
+  {
+    "revision": "f783f12921a2cf5aace94a0a0f133107",
+    "url": "/musalink/static/media/close.f783f129.svg"
+  },
+  {
+    "revision": "7fa87351dce804f27280e81cc9faf95f",
+    "url": "/musalink/static/media/deezer-circle.7fa87351.svg"
+  },
+  {
+    "revision": "1bae49a499f68120fb6ba894d2a15e81",
+    "url": "/musalink/static/media/pause.1bae49a4.svg"
+  },
+  {
+    "revision": "4b2e91e8aa3f9b0aa5c74aa725e5d346",
+    "url": "/musalink/static/media/play.4b2e91e8.svg"
+  },
+  {
+    "revision": "3e2ba66d1517bbc97140e40aab30f398",
+    "url": "/musalink/static/media/spotify.3e2ba66d.svg"
+  }
+]);
